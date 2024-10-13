e1949e843a8ba9cd4a027e5a93066a527cbf74df
Alexandre-CES <alexandreces666@gmail.com>
2024-10-13 19:15:51 -0300

    exercicio

---
+++ exercicio_aula_4.sql
@@ -0,0 +1,146 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: localhost    Database: faculdade
+-- ------------------------------------------------------
+-- Server version	8.0.37-0ubuntu0.23.10.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aluno`
+--
+
+DROP TABLE IF EXISTS `aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aluno` (
+  `matricula` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `data_nascimento` date DEFAULT NULL,
+  PRIMARY KEY (`matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `aluno_disciplina`
+--
+
+DROP TABLE IF EXISTS `aluno_disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aluno_disciplina` (
+  `aluno_matricula` varchar(255) NOT NULL,
+  `disciplina_nome` varchar(255) NOT NULL,
+  KEY `aluno_matricula` (`aluno_matricula`),
+  KEY `disciplina_nome` (`disciplina_nome`),
+  CONSTRAINT `aluno_disciplina_ibfk_1` FOREIGN KEY (`aluno_matricula`) REFERENCES `aluno` (`matricula`),
+  CONSTRAINT `aluno_disciplina_ibfk_2` FOREIGN KEY (`disciplina_nome`) REFERENCES `disciplina` (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `avaliacao`
+--
+
+DROP TABLE IF EXISTS `avaliacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `avaliacao` (
+  `cpf_professor` varchar(255) NOT NULL,
+  `nota` int DEFAULT NULL,
+  `comentario` text,
+  `data_hora` datetime NOT NULL,
+  KEY `cpf_professor` (`cpf_professor`),
+  CONSTRAINT `avaliacao_ibfk_1` FOREIGN KEY (`cpf_professor`) REFERENCES `professor` (`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `departamento`
+--
+
+DROP TABLE IF EXISTS `departamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `departamento` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `local` varchar(255) NOT NULL,
+  `cpf_professor_chef` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_professor_chef` (`cpf_professor_chef`),
+  CONSTRAINT `fk_professor_chef` FOREIGN KEY (`cpf_professor_chef`) REFERENCES `professor` (`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `disciplina`
+--
+
+DROP TABLE IF EXISTS `disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `disciplina` (
+  `nome` varchar(255) NOT NULL,
+  `carga_horaria` int NOT NULL DEFAULT '40',
+  `ementa` text,
+  PRIMARY KEY (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `professor`
+--
+
+DROP TABLE IF EXISTS `professor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `professor` (
+  `cpf` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `contato` varchar(255) DEFAULT NULL,
+  `inicio_contrato` date NOT NULL,
+  `departamento_id` int NOT NULL,
+  PRIMARY KEY (`cpf`),
+  KEY `fk_departamento_id` (`departamento_id`),
+  CONSTRAINT `fk_departamento_id` FOREIGN KEY (`departamento_id`) REFERENCES `departamento` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `professor_disciplina`
+--
+
+DROP TABLE IF EXISTS `professor_disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `professor_disciplina` (
+  `cpf_professor` varchar(255) NOT NULL,
+  `nome_disciplina` varchar(255) NOT NULL,
+  KEY `cpf_professor` (`cpf_professor`),
+  KEY `nome_disciplina` (`nome_disciplina`),
+  CONSTRAINT `professor_disciplina_ibfk_1` FOREIGN KEY (`cpf_professor`) REFERENCES `professor` (`cpf`),
+  CONSTRAINT `professor_disciplina_ibfk_2` FOREIGN KEY (`nome_disciplina`) REFERENCES `disciplina` (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-13 19:13:46
